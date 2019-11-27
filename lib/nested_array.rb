ddfab8c7d4aad9878d1927670277692bb8b175a2
honorable-exception-1170 <aliciachao@gmail.com>
2019-11-27 18:18:21 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -28,22 +28,22 @@ def array_literal_matrix
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
 array2 = [
-  ENNET_HOUSE,
-  ENFIELD_TENNIS_ACADEMY
+  ENNET_HOUSE.sort!,
+  ENFIELD_TENNIS_ACADEMY.sort!
 ]
-array2[0].sort!
-array2[1].sort!
+# array2[0].sort!
+# array2[1].sort!
 array2
 end
 
 def matrix_lookup(matrix, row, column)
   # Return the matrix's content at row and column
-  return  matrix[row][content]
+  return  matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
-  matrix[row][content] = new_value
+  matrix[row][column] = new_value
   return matrix
 end
