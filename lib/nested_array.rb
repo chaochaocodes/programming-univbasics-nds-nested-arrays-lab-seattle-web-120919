656bca45a09b7d373edc8eb7e3c26d46b9851a63
honorable-exception-1170 <aliciachao@gmail.com>
2019-11-27 18:08:57 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -19,19 +19,43 @@ ENFIELD_TENNIS_ACADEMY = [
 def assembled_matrix
   # Build an array that contains (or, "nests") the residents of The Ennet House
   # and the Enfield Tennis Academy as provided by the constants
+    array = [ ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
+    array
 end
 
 def array_literal_matrix
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
+array2 = [
+  ENNET_HOUSE = [
+    "Don Gately",
+    "Joelle van Dyne",
+    "Pat M.",
+    "Kate Gompert",
+    "Bruce Green"
+  ]
+
+  ENFIELD_TENNIS_ACADEMY = [
+    "Hal Incandenza",
+    "Lyle",
+    "Gerhard Schtitt",
+    "Mario Incandenza",
+    "Michael Pemulis"
+  ]
+]
+array2[0].sort
+array2[1].sort
 end
 
 def matrix_lookup(matrix, row, column)
   # Return the matrix's content at row and column
+  matrix[row][content]
 end
 
 def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
+  matrix[row][content] = new_value
+  matrix
 end
